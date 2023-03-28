67f038d2e9829b2947bf954d0121f8450ccc20c2
Austin <xupengyuniphone@icloud.com>
2023-03-28 21:55:45 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://beizhishang.github.io',
-    base: '/astro/',
+    base: '/astro/dist',
 });
